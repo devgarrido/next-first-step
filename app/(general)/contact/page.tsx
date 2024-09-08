869780cd81dadf6cbcb74552d6a7fbe165ece05a
Antonio Garrido Garrido <desarrollo@garrido.pro>
2024-09-08 09:12:49 +0200

    add lorem in contact

--- app/(general)/contact/page.tsx
+++ app/(general)/contact/page.tsx
@@ -3,6 +3,7 @@ export default function ContactPage() {
     return (
         <div className="flex flex-col items-center p-24">
             <span className="text-7xl">Página de Contacto</span>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat, suscipit laboriosam consectetur eius nemo sequi. Eaque eius consequuntur placeat quidem hic fugit, nihil est nobis fuga quasi et natus aut!</p>
         </div>
     );
 }
